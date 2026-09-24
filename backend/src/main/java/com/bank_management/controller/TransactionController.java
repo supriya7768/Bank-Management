1544a7841082194884916e5f95c2d0beb2804c4f
supriya7768 <mahajansupriya77@gmail.com>
2026-09-24 10:19:18 +0530

    Sorting Transaction in descending order

--- backend/src/main/java/com/bank_management/controller/TransactionController.java
+++ backend/src/main/java/com/bank_management/controller/TransactionController.java
@@ -5,6 +5,7 @@ import com.bank_management.service.TransactionService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -22,8 +23,13 @@ public class TransactionController {
             @PathVariable Long accountId,
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "10") int size) {
-
-        Pageable pageable = PageRequest.of(page, size);
+        if (page < 0) {
+            throw new IllegalArgumentException("Page number cannot be negative");
+        }
+        if (size <= 0 || size > 100) {
+            throw new IllegalArgumentException("Page size must be between 1 and 100");
+        }
+        Pageable pageable = PageRequest.of(page, size,  Sort.by(Sort.Direction.DESC, "createdAt"));
         return transactionService.getTransactionsByAccount(accountId, pageable);
     }
 }
